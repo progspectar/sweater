5719108c4455b8fcf6fb2bb3312db6d4a63ccf74
progspectar <progspectar@gmail.com>
2021-08-20 00:59:37 +0700

    add db migration

--- src/main/java/com/example/sweater/domain/Role.java
+++ src/main/java/com/example/sweater/domain/Role.java
@@ -5,6 +5,9 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
     USER, ADMIN;
 
+    Role() {
+    }
+
     @Override
     public String getAuthority() {
         return name();
